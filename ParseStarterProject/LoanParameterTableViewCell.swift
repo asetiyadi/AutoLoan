e44b747368915499e88bfdafd9ffd15d77911ef4
Andi Setiyadi <setiyadi@us.ibm.com>
2015-09-22 22:52:33 -0600

    Initial commit

---
+++ ParseStarterProject/LoanParameterTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LoanParameterTableViewCell.swift
+//  Auto Loan
+//
+//  Created by Andi Setiyadi on 9/22/15.
+//  Copyright © 2015 Parse. All rights reserved.
+//
+
+import UIKit
+
+class LoanParameterTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
